92afe57522f902941820cc99f7e1a84f0f82851d
Vanessa Nohely Arrambide Escamilla <arrambidenohely2003@gmail.com>
2024-04-11 14:28:19 -0600

    funcion operaciones
    se creo una funcion operaciones

--- practicaGit.py
+++ practicaGit.py
@@ -1,6 +1,21 @@
+# Funcion Tabla
 def tabla(numero):
     for x in range(1,11):
         print(f"{numero} x {x} = {numero*x}")
 
 # Prueba de funcion tabla
 tabla(9)
+
+#Funcion operaciones
+def operaciones(operacion,num1,num2):
+    if operacion == "suma":
+        print(f"{num1} + {num2} = {num1+num2}")
+    elif operacion == "resta":
+        print(f"{num1} - {num2} = {num1-num2}")
+    elif operacion == "multiplicacion":
+        print(f"{num1} x {num2} = {num1*num2}")
+    elif operacion == "division":
+        print(f"{num1} / {num2} = {num1/num2}")
+
+#prueba funcion operaciones
+operaciones("division",3,4)
